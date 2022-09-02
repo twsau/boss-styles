44adb70ff97fb012285aac679c1bccff5ec574d7
twsau <contact.twsau@gmail.com>
2022-09-02 14:44:29 +0100

    minify css during build process

---
+++ minify.js
@@ -0,0 +1,22 @@
+const postcss = require("postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+const fs = require("fs");
+
+const minifyCss = async (css) => {
+  const output = await postcss([cssnano, autoprefixer]).process(css);
+  const minifiedCss = await output;
+  return minifiedCss;
+};
+
+const writeToFile = (css) => {
+  fs.writeFile("./dist/global.min.css", css, (error) => {
+    if (error) return console.error(error);
+    console.log("success!");
+  });
+};
+
+fs.readFile("./dist/global.css", "Utf8", (error, data) => {
+  if (error) return console.error(error);
+  minifyCss(data).then(({ css }) => writeToFile(css));
+});
